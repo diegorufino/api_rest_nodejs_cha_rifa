878003d8ed0545f15550b954b1f87968e4bd572e
Diego Rufino <diegorufino1@gmail.com>
2021-04-20 22:47:46 -0300

    add header in cors

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(bodyParser.json()) //json de entrada no body
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); 
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'); 
     app.use(cors())
     next();
 })
